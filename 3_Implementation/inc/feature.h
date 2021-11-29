81529945fc59fb491f90f6221629a28940444fe2
Gokul0808 <94180547+Gokul0808@users.noreply.github.com>
2021-11-29 21:40:18 +0530

    Update feature.h

--- 3_Implementation/inc/feature.h
+++ 3_Implementation/inc/feature.h
@@ -1,1 +1,16 @@
+/**
+ * @file feature.h
+ * @author Gokulakrishnan
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
+#ifndef __AVR_ATmega328__
+#define __AVR_ATmega328__
+
+#include <avr/io.h>
+#include <util/delay.h>
