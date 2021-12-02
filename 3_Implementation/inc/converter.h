ba21f1e5d39e577879e4b20563cd682a33ab5e5f
Gokul0808 <94180547+Gokul0808@users.noreply.github.com>
2021-12-02 15:22:19 +0530

    Update and rename feature.h to converter.h

---
+++ 3_Implementation/inc/converter.h
@@ -0,0 +1,20 @@
+/**
+ * @file feature.h
+ * @author Gokulakrishnan
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __AVR_ATmega328__ 
+    #define __AVR_ATmega328__
+#endif
+
+#include<avr/io.h>
+#include<util/delay.h>
+#define sei
+#define cli
+#define delay_ms
